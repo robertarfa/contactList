bad84d187d5cd3c4ecc5b072659d85dd4d6d08d8
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-07-15 08:22:06 -0300

    contactLis

--- app/src/main/java/com/example/contactlist/ContactAdapter.kt
+++ app/src/main/java/com/example/contactlist/ContactAdapter.kt
@@ -3,6 +3,8 @@ package com.example.contactlist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -22,10 +24,21 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
         holder.bind(list[position])
     }
 
+    fun updateList(list: List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        fun bind(contact : Contact){
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhoto: ImageView = itemView.findViewById(R.id.iv_photo)
 
+        fun bind(contact : Contact){
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
         }
     }
 
--- app/src/main/java/com/example/contactlist/MainActivity.kt
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
+    }
+
+    private fun bindView(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 2",
+                    "(00) 5555-6666",
+                    "img.png"
+                )
+            )
+        )
     }
 }
