59c76fb3d85e0b266c0fc9b6ce8125246e1bd6a1
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-07-19 08:13:55 -0300

    DrawerMenu

--- app/src/main/java/com/example/contactlist/MainActivity.kt
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -2,6 +2,15 @@ package com.example.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,10 +22,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindView()
         updateList()
+    }
+
+    private fun initDrawer(){
+        val drawerLayout=findViewById<View>(R.id.drawer_layout) as DrawerLayout
+
+        val toolbar =   findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle  =   ActionBarDrawerToggle(this, drawerLayout , toolbar ,R.string.open_drawer,R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
 
     private fun bindView(){
@@ -36,8 +58,73 @@ class MainActivity : AppCompatActivity() {
                     "Roberta Aguilar 2",
                     "(00) 5555-6666",
                     "img.png"
-                )
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+                Contact(
+                    "Roberta Aguilar 1",
+                    "(00) 5555-6666",
+                    "img.png"
+                ),
+
             )
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater =  menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    private fun showToast(message:String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_menu_1->{
+                showToast("Item 1")
+                true
+            }
+            R.id.item_menu_2 ->{
+                showToast("Item 2")
+                true
+            }
+            else    ->super.onOptionsItemSelected(item)
+        }
     }
 }
